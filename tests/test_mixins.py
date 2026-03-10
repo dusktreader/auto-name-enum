6f3bdafca3ad0040f4a9a2e87a4fb32cefb7cf26
Tucker Beck <tucker.beck@gmail.com>
2026-03-09 22:05:29 -0700

    feat: add `display_name` to autodoc()
    This adds the `display_name` kwarg to autodoc() capability.
    
    Also fixed issue with mixins not respecting autodoc.

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -1,4 +1,4 @@
-from auto_name_enum import AutoNameEnum, auto, LowerCaseMixin, TitleCaseMixin
+from auto_name_enum import AutoNameEnum, auto, autodoc, LowerCaseMixin, TitleCaseMixin
 
 
 class LowerEnum(AutoNameEnum, LowerCaseMixin):
@@ -7,11 +7,28 @@ class LowerEnum(AutoNameEnum, LowerCaseMixin):
     HUTT = auto()
 
 
+class LowerAutoDocEnum(AutoNameEnum, LowerCaseMixin):
+    JAWA = autodoc(description="A small, rodent-like alien from Tatooine", display_name="Jawa (Tatooine)")
+    EWOK = autodoc(description="A furry, diminutive alien from the forest moon of Endor", display_name="Ewok (Endor)")
+    HUTT = autodoc(description="A large, slug-like alien from Nal Hutta", display_name="Hutt (Nal Hutta)")
+
+
 class TestLowerCaseMixin:
     def test_auto(self):
         assert LowerEnum.JAWA.value == "jawa"
         assert LowerEnum.EWOK.value == "ewok"
         assert LowerEnum.HUTT.value == "hutt"
+
+    def test_autodoc(self):
+        assert LowerAutoDocEnum.JAWA.value == "jawa"
+        assert LowerAutoDocEnum.JAWA.description == "A small, rodent-like alien from Tatooine"
+        assert LowerAutoDocEnum.JAWA.display_name == "Jawa (Tatooine)"
+        assert LowerAutoDocEnum.EWOK.value == "ewok"
+        assert LowerAutoDocEnum.EWOK.description == "A furry, diminutive alien from the forest moon of Endor"
+        assert LowerAutoDocEnum.EWOK.display_name == "Ewok (Endor)"
+        assert LowerAutoDocEnum.HUTT.value == "hutt"
+        assert LowerAutoDocEnum.HUTT.description == "A large, slug-like alien from Nal Hutta"
+        assert LowerAutoDocEnum.HUTT.display_name == "Hutt (Nal Hutta)"
 
 
 class TitleEnum(AutoNameEnum, TitleCaseMixin):
@@ -20,8 +37,25 @@ class TitleEnum(AutoNameEnum, TitleCaseMixin):
     HUTT = auto()
 
 
+class TitleAutoDocEnum(AutoNameEnum, TitleCaseMixin):
+    JAWA = autodoc(description="A small, rodent-like alien from Tatooine", display_name="Jawa (Tatooine)")
+    EWOK = autodoc(description="A furry, diminutive alien from the forest moon of Endor", display_name="Ewok (Endor)")
+    HUTT = autodoc(description="A large, slug-like alien from Nal Hutta", display_name="Hutt (Nal Hutta)")
+
+
 class TestTitleCaseMixin:
     def test_auto(self):
         assert TitleEnum.JAWA.value == "Jawa"
         assert TitleEnum.EWOK.value == "Ewok"
         assert TitleEnum.HUTT.value == "Hutt"
+
+    def test_autodoc(self):
+        assert TitleAutoDocEnum.JAWA.value == "Jawa"
+        assert TitleAutoDocEnum.JAWA.description == "A small, rodent-like alien from Tatooine"
+        assert TitleAutoDocEnum.JAWA.display_name == "Jawa (Tatooine)"
+        assert TitleAutoDocEnum.EWOK.value == "Ewok"
+        assert TitleAutoDocEnum.EWOK.description == "A furry, diminutive alien from the forest moon of Endor"
+        assert TitleAutoDocEnum.EWOK.display_name == "Ewok (Endor)"
+        assert TitleAutoDocEnum.HUTT.value == "Hutt"
+        assert TitleAutoDocEnum.HUTT.description == "A large, slug-like alien from Nal Hutta"
+        assert TitleAutoDocEnum.HUTT.display_name == "Hutt (Nal Hutta)"
